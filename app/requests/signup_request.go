5a2c3526c69ed7b2f015f23f3321c595bf7030f7
hongxuechen <hongxuechen@hongxuechendeMac-mini.local>
2022-05-19 15:07:45 +0800

    JSON 解析和验证器封装

--- app/requests/signup_request.go
+++ app/requests/signup_request.go
@@ -22,17 +22,7 @@ func ValidateSignupPhoneExist(data interface{},c *gin.Context) map[string][]stri
 			"digits:手机号长度必须为 11 位的数字",
 		},
 	}
-
-	// 配置初始化
-	opts := govalidator.Options{
-		Data:          data,
-		Rules:         rules,
-		TagIdentifier: "valid", // 模型中的struct 标签标识符
-		Messages:      messages,
-	}
-
-	// 开始验证
-	return govalidator.New(opts).ValidateStruct()
+	return validate(data,rules,messages)
 }
 
 type SignupEmailExistRequest struct {
@@ -53,14 +43,5 @@ func ValidateSignupEmailExist(data interface{},c *gin.Context) map[string][]stri
 			"email:Email 格式不正确，请提供有效的邮箱地址",
 		},
 	}
-	// 配置初始化
-	opts := govalidator.Options{
-		Data:          data,
-		Rules:         rules,
-		TagIdentifier: "valid", // 模型中的 Struct 标签标识符
-		Messages:      messages,
-	}
-
-	// 开始验证
-	return govalidator.New(opts).ValidateStruct()
+	return validate(data,rules,messages)
 }
