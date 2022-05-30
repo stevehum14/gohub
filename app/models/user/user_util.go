cb4693652927a57ab8a000ea7ac2c427cf58a729
stevehum <stevehum@192.168.0.7>
2022-05-30 22:17:04 +0800

    手机 + 短信验证码登录

--- app/models/user/user_util.go
+++ app/models/user/user_util.go
@@ -1,6 +1,8 @@
 package user
 
-import "gohub/pkg/database"
+import (
+	"gohub/pkg/database"
+)
 
 // IsEmailExist 判断 email 已被注册
 func IsEmailExist(email string) bool  {
@@ -14,4 +16,19 @@ func IsPhoneExist(phone string) bool  {
 	var count int64
 	database.DB.Model(User{}).Where("phone = ?",phone).Count(&count)
 	return count > 0
+}
+
+// GetByPhone 通过手机号来获取用户
+func GetByPhone(phone string)(userModel User)  {
+	database.DB.Where("phone = ?",phone).First(&userModel)
+	return
+}
+
+//  通过 手机号/email/用户名来获取用户
+func GetByMulti(loginID string)(userModel User)  {
+	database.DB.Where("phone = ?",loginID).
+		Or("emial = ?",loginID).
+		Or("name = ? ",loginID).
+		First(&userModel)
+	return
 }
