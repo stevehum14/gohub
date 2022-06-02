b635c7396d4662b38d0b822ad37c0e4698178784
hongxuechen <hongxuechen@hongxuechendeMac-mini.local>
2022-06-02 17:41:19 +0800

    命令行模式

--- main.go
+++ main.go
@@ -1,65 +1,62 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"github.com/gin-gonic/gin"
+	"gohub/app/cmd"
 	"gohub/bootstrap"
-	btsConfig "gohub/config"
+	btsConig "gohub/config"
 	"gohub/pkg/config"
-	"net/http"
+	"gohub/pkg/console"
+	"os"
+
+	"github.com/spf13/cobra"
 )
 
-func init()  {
+func init() {
 	// 加载 config 目录下的配置信息
-	btsConfig.Initialize()
+	btsConig.Initialize()
 }
 
-func main()  {
-	// 配置初始化，依赖命令行 --env 参数
-	var env string
-	flag.StringVar(&env,"env","","加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
-	flag.Parse()
-	config.InitConfig(env)
+func main() {
 
-	// 初始化 Logger
-	bootstrap.SetupLogger()
+	// 应用的主入口，默认调用 cmd.CmdServe 命令
+	var rootCmd = &cobra.Command{
+		Use:   config.Get("app.name"),
+		Short: "A simple forum project",
+		Long:  `Default will run "serve" command, you can use "-h" flag to see all subcommands`,
 
-	// 设置 gin 的运行模式，支持 debug, release, test
-	// release 会屏蔽调试信息，官方建议生产环境中使用
-	// 非 release 模式 gin 终端打印太多信息，干扰到我们程序中的 Log
-	// 故此设置为 release，有特殊情况手动改为 debug 即可
-	gin.SetMode(gin.ReleaseMode)
+		// rootCmd 的所有子命令都会执行以下代码
+		PersistentPreRun: func(command *cobra.Command, args []string) {
 
-	// new 一个 Gin Engine 实例
-	router := gin.New()
+			// 配置初始化，依赖命令行 --env 参数
+			config.InitConfig(cmd.Env)
 
-	// 初始化 db
-	bootstrap.SetupDB()
+			// 初始化 Logger
+			bootstrap.SetupLogger()
 
-	// 初始化 Redis
-	bootstrap.SetupRedis()
+			// 初始化数据库
+			bootstrap.SetupDB()
 
-	// 初始化路由绑定
-	 bootstrap.SetupRoute(router)
+			// 初始化 Redis
+			bootstrap.SetupRedis()
 
-	//router.GET("/test_auth", middlewares.AuthJWT(), func(c *gin.Context) {
-	//	userModel := auth.CurrentUser(c)
-	//	response.Data(c, userModel)
-	//})
+			// 初始化缓存
+		},
+	}
 
+	// 注册子命令
+	rootCmd.AddCommand(
+		cmd.CmdServe,
+	)
 
-	// 注册一个路由
-	router.GET("/", func(c *gin.Context) {
-		// 以 json 格式响应
-		c.JSON(http.StatusOK,gin.H{
-			"Hello":"World!",
-		})
-	})
-	// 运行服务
-	err := router.Run(":" + config.Get("app.port"))
-	if err != nil {
-		// 错误处理，端口被占用了或者其他错误
-		fmt.Println(err.Error())
+	// 配置默认运行 Web 服务
+	cmd.RegisterDefaultCmd(rootCmd, cmd.CmdServe)
+
+	// 注册全局参数，--env
+	cmd.RegisterGlobalFlags(rootCmd)
+
+	// 执行主命令
+	if err := rootCmd.Execute(); err != nil {
+		console.Exit(fmt.Sprintf("Failed to run app with %v: %s", os.Args, err.Error()))
 	}
 }
